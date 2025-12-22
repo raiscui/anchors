c730ea442c68246b5d022612f3aacebc0a30a9f2
Rais <vdcoolzi@gmail.com>
2025-12-22 21:33:47 +0800

    允许 incomplete_features 警告

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(negative_impls)]
 #![feature(exact_size_is_empty)]
 #![feature(specialization)]
+#![allow(incomplete_features)]
 // #![feature(option_result_contains)] //for with_anchor func
 #![feature(iter_collect_into)]
 #![feature(auto_traits)]
